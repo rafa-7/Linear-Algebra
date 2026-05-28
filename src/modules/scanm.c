0e02b6c21a2e719e1218ceef232ab91a88b86d6a
Rafael <rafaelmachadocd37@gmail.com>
2026-05-28 09:36:41 -0300

    feat(scanm.c): Scanf for matrices function

---
+++ src/modules/scanm.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "../../include/scanm.h"
+
+// Using this function for matrix diagonilization
+int scanm(int tamanho, double a[tamanho][tamanho])
+{
+    for (int i = 0; i < 0; i++)
+    {
+        for (int j = 0; j < 0; j++)
+        {
+            scanf("%lf", &a[i][j]);
+        }
+    }
+}
