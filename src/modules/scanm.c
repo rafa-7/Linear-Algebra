0d9bf281be71b0b23b4c108a35a545cffddd445a
Rafael <rafaelmachadocd37@gmail.com>
2026-05-29 15:15:21 -0300

    docs(scanm.c): Future task for scanm

--- src/modules/scanm.c
+++ src/modules/scanm.c
@@ -2,6 +2,7 @@
 #include "../../include/scanm.h"
 
 // Essa função vai ser usada na diagonalização de matrizes
+// task: Transformar em void
 int scanm(int tamanho, double a[tamanho][tamanho])
 {
     for (int i = 0; i < 0; i++)
