f9df91b8d72fb8a06aa4e71d3b511d4e77178b29
Rafael <rafaelmachadocd37@gmail.com>
2026-06-08 11:57:16 -0300

    feat(determinant.c): Finally using Laplace for >4 matrices.

--- src/modules/determinant.c
+++ src/modules/determinant.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
-
-// Usando o abs para módulo
-#include <stdlib.h>
 #include "../../include/determinant.h"
 
 // O determinante pode apenas ser calculado para matrizes quadradas
+// Essa função vai ser utilizada para os seguintes problemas:
+// - Checking for injectivity, surjectivity, and bijectivity.
+// - Determination of Bases
+// - Eigenvalue and Eigenvector Calculation
+// - Matrix Diagonalization
 
 /*
 Para chamar a função, vamos utilizar uma matriz 4x4 como exemplo:
 
-    int matrizTeste[2][2] =
+    int matrizTeste[4][4] =
     {
         {1, 2, 3.2, 5},
         {3, 4, 290.23, 3.14159},
@@ -20,6 +22,23 @@ Para chamar a função, vamos utilizar uma matriz 4x4 como exemplo:
     printf("det(A)=%.1lf\n",det(2, matrizTeste));
     Isso retorna o determinante
 */
+
+void subMatrix(int ordem, double a[ordem][ordem], subMatrix[tamanho][tamanho], int colunaDel, int ordemAtual)
+{
+    int linhaSub = 0;
+
+    for (int i = 1; i < ordem; i++)
+    {
+        int colunaSub = 0;
+        for (int j = 0; j < ordem; j++)
+        {
+            if (j == colunaDel) continue;
+            subMatrix[linhaSub][colunaSub] = a[i][j];
+            colunaSub++;
+        }
+        linhaSub++;
+    }
+}
 
 double det(int ordem, double a[ordem][ordem])
 {
@@ -39,20 +58,23 @@ double det(int ordem, double a[ordem][ordem])
     }
     else if (ordem >= 4)
     {
-        // Eliminação Gaussiana
-        for (int i = 0; i < ordem; i++)
+        // Teorema de Laplace 
+        // Escolheremos sempre a primeira linha pra facilitar o cálculo
+        double resultadoDet = 0.0;
+        int sinal = 1;
+
+        double matrixRedu[ordem][ordem];
+
+        
+        for (int j = 0; j < ordem; j++)
         {
-            for (int j = i + 1; j < ordem; j++)
-            {
-                // A intenção é definir o maior pivô pra não termos divisão por zero aqui
-                if (a[j][i] > a[i][i])
-                {
+            subMatrix(ordem, a, matrixRedu, j);
+            double elemento = a[0][j];
 
-                }
-            }
+            resultadoDet += sinal * elemento * det(ordem - 1, matrixRedu);
+            sinal = -sinal;
         }
-
-
+        return resultadoDet;
     }
     else
     {
