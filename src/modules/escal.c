2677cdeadfe81b59fb1200fd1d5a427927ffcfd6
YURI-gomes062 <yurioficial892@gmail.com>
2026-06-15 14:39:39 -0300

    feat(escal.c): Adding code to the escal.c function

--- src/modules/escal.c
+++ src/modules/escal.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include "../../include/escal.h"
+// Eliminação de Gauss(Escalonamento).
 
-int escal(){
+// A função deverá receber uma matriz de tamanho definido:
+
+/*Exemplo:
+
+    int matriz[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    }
+
+    escal(matriz[3][3]);
+*/
+int escal(double a[tamanho][tamanho]){
+    scanf("", );
 
 
     return 0;
-}la
+}
