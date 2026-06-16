5215cf6d6b470361714d7a5d527c9d0bbd20945f
Rafael <rafaelmachadocd37@gmail.com>
2026-06-16 14:38:24 -0300

    feat(main.c): TUI initials

--- src/main.c
+++ src/main.c
@@ -1,60 +1,101 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include "../include/det.h"
 #include "../include/scanm.h"
 #include "../include/autov.h"
 
-/**
- *    .____    .__                                             
- *    |    |   |__| ____   ____ _____ _______                  
- *    |    |   |  |/    \_/ __ \\__  \\_  __ \                 
- *    |    |___|  |   |  \  ___/ / __ \|  | \/                 
- *    |_______ \__|___|  /\___  >____  /__|                    
- *            \/       \/     \/     \/                        
- *       _____  .__                       ___.                 
- *      /  _  \ |  |    ___________   ____\_ |______________   
- *     /  /_\  \|  |   / ___\_  __ \_/ __ \| __ \_  __ \__  \  
- *    /    |    \  |__/ /_/  >  | \/\  ___/| \_\ \  | \// __ \_
- *    \____|__  /____/\___  /|__|    \___  >___  /__|  (____  /
- *            \/     /_____/             \/    \/           \/ 
-*/
 
 int main()
 {
-    // Matriz pra teste 4x4
-    double test4[4][4] = {
-        {1, 2, 3, 4},
-        {4, 5, 6, 7},
-        {7, 8, 9, 10},
-        {11, 12, 13, 14}
-    };
+    bool tui = true;
+    // Limpa a tela antes
+    system("clear");
 
-    // Matriz pra teste 3x3
-    double test3[3][3] = {
-        {1, 2, 3},
-        {4, 5, 6},
-        {7, 8, 9}
-    };
+    // TUI (Terminal User-Interface)
+    while (tui)
+    {
+        int escolha;
 
-    // Matriz pra teste 2x2
-    double test2[2][2] = {
-        {1, 2},
-        {3, 4}
-    };
+        
 
-    // Matriz pra teste 1x1
-    double test1[1][1] = {
-        {1}
-    };
+        printf("\n"
+            "       _____  .__                ___.                  \n"
+            "      /  _  \\ |  |    ____   ____\\_ |______________   \n"
+            "     /  /_\\  \\|  |   / ___\\_/ __ \\| __ \\_  __ \\__  \\  \n"
+            "    /    |    \\  |__/ /_/  >  ___/| \\_\\ \\  | \\// __ \\_\n"
+            "    \\____|__  /____/\\___  / \\___  >___  /__|  (____  /\n"
+            "            \\/     /_____/      \\/    \\/           \\/ \n"
+            "    .____     .__                                      \n"
+            "    |    |    |__| ____   ____ _____ _______           \n"
+            "    |    |    |  |/    \\_/ __ \\\\__  \\\\_  __ \\          \n"
+            "    |    |___|  |   |  \\  ___/ / __ \\|  | \\/          \n"
+            "    |_______ \\__|___|  /\\___  >____  /__|             \n"
+            "            \\/       \\/     \\/     \\/                 \n"
+        "\n");
+    
+        printf("O que deseja fazer?\n");    
+        printf("(1) Resolver sistemas lineares\n");
+        printf("(2) Verificar Injetividade, Sobrejetividade e Bijetividade\n");
+        printf("(3) Determinar bases\n");
+        printf("(4) Calcular autovalores e/ou autovetores\n");
+        printf("(5) Diagonalização de matrizes\n");
+        printf("(0) Finalizar programa");
+        printf("\n> ");
+        scanf("%d", &escolha);
 
-    printf("O determinante dessa matriz é equivalente a %lf\n",det(2, test2));
-
-
-    double res1;
-    double res2;
-    autova(2, test2, &res1, &res2);
-
-    printf("Os autovalores são: A1 = %lf\nA = 2 %lf", res1, res2);
-
-    return 0;
+        switch (escolha)
+        {
+            case 1:
+            {
+                break;
+            }
+                
+            case 2:
+            {
+                break;
+            }
+                
+            case 3:
+            {
+                break;
+            }
+            
+            case 4:
+            {
+                break;
+            }
+            
+            case 5:
+            {
+                break;
+            }
+                
+            case 0:
+            {
+                printf("Programa encerrado.\n\n");
+                tui = false;
+                break;
+            }
+                
+            default:
+            {
+                printf("Digite um valor válido!\n\n");
+                tui = false;
+                break;
+            }
+        } 
+    }
 }
+
+
+/*
+⠀⠀⠀⢸⣦⡀⠀⠀⠀⠀⢀⡄
+⠀⠀⠀⢸⣏⠻⣶⣤⡶⢾⡿⠁
+⠀⠀⣀⣼⠷⠀⠀⠁ 𝑺𝑬𝑬 𝒀𝑶𝑼 𝑺𝑷𝑨𝑪𝑬
+⠴⣾⣯⣅⣀⠀⠀⠀⠈⢻⣦⡀𝑪𝑶𝑾𝑩𝑶𝒀 . . .
+⠀⠀⠀⠉⢻⡇⣤⣾⣿⣷⣿⠏⠉
+⠀⠀⠀⠀⠸⣿⡿⠏⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠟⠁⠀⠀⠀⠀ ⠀⠀
+*/
