35dfba803dab118904499ec1d3ee0dc94e61ea16
Rafael <rafaelmachadocd37@gmail.com>
2026-06-17 14:13:36 -0300

    feat(printm.c): printm function is now void

--- src/modules/printm.c
+++ src/modules/printm.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 // Necessário pra diagonalização
-int printm(int tamanho, double a[tamanho][tamanho])
+void printm(int tamanho, double a[tamanho][tamanho])
 {
     for (int i = 0; i < tamanho; i++)
     {
