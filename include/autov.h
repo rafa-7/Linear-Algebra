95ae23cf63c4efd6285056c3ab2d599e2b53708a
Rafael <rafaelmachadocd37@gmail.com>
2026-06-18 15:26:48 -0300

    feat(autov.h): Now using type raizes for parameters and autova

--- include/autov.h
+++ include/autov.h
@@ -7,6 +7,6 @@ typedef struct
     double delta2;
 } raizes;
 
-void autova(int ordem, double matriz[ordem][ordem], double *autova1, double *autova2);
+raizes autova(int ordem, double matriz[ordem][ordem], double *autova1, double *autova2);
 
-double autove(int ordem, double matriz[ordem][ordem], double *autova1, double *autova2);
+double autove(int ordem, double matriz[ordem][ordem], raizes valores);
