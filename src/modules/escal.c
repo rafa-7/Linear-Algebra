b0d4c0be6b16c2344b0d4bc1bbacaf60984f42f3
YURI_c137 <yuri.lu312@gmail.com>
2026-06-22 15:52:48 -0300

    refactor(escal.c)alteração na função escalonamento

--- src/modules/escal.c
+++ src/modules/escal.c
@@ -1,17 +1,34 @@
 #include <stdio.h>
 
+// !------------------------------------------------------------------
+// TODO : Método do Escalonamento para resolução de sistemas lineares.
+// !------------------------------------------------------------------
+// TODO : Tamanhos aceitos: 2x2, 3x3, 3x2, 2x3.
+// !------------------------------------------------------------------
+
+void identificar_pivos(int linhas, int colunas, int matri[linhas][colunas]);
+
+// ---------------------
 int matriz[3][3] = {
         {1, 2, 3},
         {4, 5, 6},
         {7, 8, 9}
-    }
+};
+// ---------------------
+int main(){
+
+
+identificar_pivos(3, 3, matriz);
+
+    return 0;
+}
 
 int escal(){ // Método de resolução de sistemas lineares (Eliminação de Gauss);
 
 
 // --------------------Chamadas de Funções-----------------------------------------------
 // --------------------Sequência de Passos-----------------------------------------------
-/*1°*/    identificar_pivos(matriz, 3); // Identifica o próximo pivô que sera utilizado na matriz.
+/*1°*/    identificar_pivos(linha, coluna, matriz); // Identifica o próximo pivô que sera utilizado na matriz.
 /*2°*/    encontrar_fator_multi_pivo(); // Encontra o fator do pivô para igualá-lo a 1, PELA DIVISÃO [ PIVO = número que eu quero zerar / número acima ]
 /*3°*/    zera_elementos_abaixo_pivo(); // Zera os elementos embaixo do pivô atual
 // -------Realizar os passos acima para deixar a matriz escalonada--------
@@ -22,17 +39,23 @@ int escal(){ // Método de resolução de sistemas lineares (Eliminação de Gau
     return 0;
 }
 
+void identificar_pivos(int linhas, int colunas, matri[linhas][colunas]){ // função com responsabilidade de identificar o maior número em valor absoluto para ser pivô.
+   
+    int maior_indice = matri[0][0]; // essa váriavel vai segurar o primeiro indice da matriz e vai comparar ele com os outos valores
 
+    for(int i = 0; i < linhas; i++){ // for para percorrer as linhas
+        for(int j = 0; j < colunas; i++){ // for para percorrer as colunas
 
-void identificar_pivos(int linhas, int colunas, matri[linhas][colunas]){ // função com responsabilidade de identificar o pivo mais simples das equações do sistema linear
-    for(int i = 0; i < linhas; i++){ // Repetição para as linhas
-        for(int j = 0; j < 3; j++){ // Repetição para as colunas
-            if(matri[linhas][colunas] == 0){
+            if(matri[i][j] == 0){
+                trocar_linha(); // TODO : Terminar de escrever essa função para validar a condicional.
+            }
+
+            if(matri[i][j] < 0 && (matri[i][j] * -1) >= maior_indice){  // condicional que verifica o valor do número absoluto e confere se ele é maior que o número contido na variavel "maior número", caso ele seja maior o valor da variavel sera substituido.
+                maior_indice = matri[i][j];
 
             }
         }
-    }
-
+   }
 }
 
 
