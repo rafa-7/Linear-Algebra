5c0ee0dfcb775218a9e9b9c912998e7447dbacc6
Rafael <rafaelmachadocd37@gmail.com>
2026-06-22 14:18:13 -0300

    feat(autov.c): Logic is almost done, just in need of ESCAL.C TO WORK

--- src/modules/autov.c
+++ src/modules/autov.c
@@ -40,9 +40,9 @@ void autova(int ordem, double matriz[ordem][ordem], raizes *autovalores)
     {
         double traco = matriz[0][0] + matriz[1][1];
         double detMatriz = det(2, matriz);
-        eq = bhaskara(1, traco, detMatriz);
+        eq = bhaskara(1, -traco, detMatriz);
         autovalores->delta1 = eq.delta1;
-        autovalores->delta1 = eq.delta2;   
+        autovalores->delta2 = eq.delta2;   
     }
     else if (ordem == 1)
     {  
@@ -57,8 +57,24 @@ void autova(int ordem, double matriz[ordem][ordem], raizes *autovalores)
 
 // Matrizes de ordem no máximo 2
 // Receber autovalores e matriz para cálculo de autovetores
-double autove(int ordem, double matriz[ordem][ordem], raizes valores)
+void autove(int ordem, double matriz[ordem][ordem], raizes *valores)
 {
+    // Qualquer número diferente de 0 se enquadra como um autovetor de matrizes 1x1
+    if (ordem == 1) return 1;
+
+    // Para o primeiro autovalor
+
+    matriz[0][0] -= valores->delta1;
+    matriz[1][1] -= valores->delta1;
+
+    // Cálculo do sistema linear gerado . . . (escalonamento)
+
+    // Para o segundo autovalor
+    matriz[0][0] -= valores.delta2;
+    matriz[1][1] -= valores.delta2;
+
+    // Cálculo do sistema linear gerado . . . (escalonamento)
+
     return 1;
 
 
