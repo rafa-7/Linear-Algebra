631eb0805b6394291f5449825db88f3422d3eeee
YURI-gomes062 <yurioficial892@gmail.com>
2026-06-25 01:58:02 -0300

    refactor(escal.c)refactor the functions.

--- src/modules/escal.c
+++ src/modules/escal.c
@@ -6,8 +6,6 @@
 // TODO : Tamanhos aceitos: 2x2, 3x3, 3x2, 2x3.
 // !------------------------------------------------------------------
 
-void identificar_pivos(int linhas, int colunas, int matri[linhas][colunas]);
-
 // ---------------------
 int matriz[3][3] = {
         {1, 2, 3},
@@ -23,44 +21,43 @@ identificar_pivos(3, 3, matriz);
     return 0;
 }
 
-int escal(){ // Método de resolução de sistemas lineares (Eliminação de Gauss);
-
-
-// --------------------Chamadas de Funções-----------------------------------------------
-// --------------------Sequência de Passos-----------------------------------------------
-/*1°*/    identificar_pivos(linha, coluna, matriz); // Identifica o próximo pivô que sera utilizado na matriz.
-/*2°*/    encontrar_fator_multi_pivo(); // Encontra o fator do pivô para igualá-lo a 1, PELA DIVISÃO [ PIVO = número que eu quero zerar / número acima ]
-/*3°*/    zera_elementos_abaixo_pivo(); // Zera os elementos embaixo do pivô atual
-// -------Realizar os passos acima para deixar a matriz escalonada--------
-/*4°*/    substituicao_retroativa(); // exemplo, sobrou 3z = 9; você deve fazer z = 9/3; RESUMINDO, descobrir o valor das incognitas por de cima para baixo, agora que descobriu o Z, consegue descobrir o Y
+// TODO :--------------------Chamadas de Funções-----------------------------------------------
+// Todo :--------------------Sequência de Passos-----------------------------------------------
+//    copiar_matriz_temporaria();
+//    identificar_pivo(); // Identifica o próximo pivô que sera utilizado na matriz.
+//    encontrar_fator_multi_pivo(); // Encontra o fator do pivô para igualá-lo a 1, PELA DIVISÃO [ PIVO = número que eu quero zerar / número acima ]
+//    zera_elementos_abaixo_pivo(); // Zera os elementos embaixo do pivô atual
+// Todo :---------Realizar os passos acima para deixar a matriz escalonada---------------------
+//    substituicao_retroativa(); // exemplo, sobrou 3z = 9; você deve fazer z = 9/3; RESUMINDO, descobrir o valor das incognitas por de cima para baixo, agora que descobriu o Z, consegue descobrir o Y
 // --------------------------------------------------------------------------------------
 
+//! ------------Variaveis-------------
+
+int matriz_alteravel[][]; // matriz que deverá compor os valores da matriz que será escalonada.
+int pivo;
+
+int copia_matriz(int linha, int coluna, int matriz[linha][coluna]){// ? Função para copiar os valores da matriz que será escalonada e inserir em uma matriz alteravel.
+    for(int i = 0; i< linha; i++){ 
+        for(int j = 0; j< coluna; j++){
+            matriz_alteravel[i][j] = matriz[linha][coluna];
+        }
+    }
+    return 0;
+}
+int identificar_pivo(int linha, int coluna, int matriz[linha][coluna]){
+
+    for(int i = 0; i< linha; i++){ //for para colunas.
+        for(int j = 0; j< coluna; j++){ // for para linhas.
+            
+            if(matriz[linha][coluna] == 0 && matriz[linha + 1][coluna] !0 ){
+                trocar_linhas(matriz[linha][coluna],matriz[linha][coluna]);
+            }
+            // ! OBS: Criar uma função que verifica se os valores abaixo do pivô estão zerados para poder definir o próximo pivô.
+        }
+    }
 
     return 0;
 }
-
-void identificar_pivos(int linhas, int colunas, matri[linhas][colunas]){ // função com responsabilidade de identificar o maior número em valor absoluto para ser pivô.
-   
-    int maior_indice = matri[0][0]; // essa váriavel vai segurar o primeiro indice da matriz e vai comparar ele com os outos valores
-
-    for(int i = 0; i < linhas; i++){ // for para percorrer as linhas
-        for(int j = 0; j < colunas; i++){ // for para percorrer as colunas
-
-            if(matri[i][j] == 0){
-                trocar_linha(); // TODO : Terminar de escrever essa função para validar a condicional.
-            }
-
-            if(matri[i][j] < 0 && (matri[i][j] * -1) >= maior_indice){  // condicional que verifica o valor do número absoluto e confere se ele é maior que o número contido na variavel "maior número", caso ele seja maior o valor da variavel sera substituido.
-                maior_indice = matri[i][j];
-
-            }
-        }
-   }
-}
-
-
-void zera_elementos_abaixo_pivo(){
-
-
+int trocar_linhas(int linha, int coluna, int matriz[linha][coluna]){
 
 }
